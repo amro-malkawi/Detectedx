a86dc8a4ede144c0ed14d02a54569e6be67b3555
andrei0807 <andrei0807@hotmail.com>
2020-10-21 16:19:01 +0800

    2020/10/21

---
+++ src/routes/test-view/component/DownloadProgress.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles({
+    root: {
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+    },
+});
+
+export default ({totalCount, downCount}) => {
+    const classes = useStyles();
+    if(totalCount === downCount) {
+        return null;
+    } else {
+        return (
+            <div className={classes.root}>
+                <LinearProgress variant="determinate" value={Math.round((downCount * 100) / totalCount)} />
+            </div>
+        )
+    }
+}
---
+++ src/routes/test-view/lib/CornerstoneViewport/index.js
@@ -0,0 +1,6 @@
+import CornerstoneViewport from './Viewport/Viewport.js';
+import ViewportOverlay from './ViewportOverlay/ViewportOverlay';
+
+export { ViewportOverlay };
+
+export default CornerstoneViewport;
