b134cb2879603cf5f5a12d4da60cca18590f8f6a
andrei0807 <andrei0807@hotmail.com>
2020-10-23 21:27:03 +0800

    2020/10/23

--- src/routes/test-view/component/LoadingIndicator.js
+++ src/routes/test-view/component/LoadingIndicator.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
 export default ({percentComplete}) => {
-    if(percentComplete === 100) {
-        return null
-    } else {
-        return (
-            <div className="imageViewerLoadingIndicator loadingIndicator">
-                <div className="indicatorContents">
-                    <h2>
-                        Loading...
-                        <i className="fa fa-spin fa-circle-o-notch fa-fw" />{' '}
-                    </h2>
-                </div>
+    return (
+        <div className="imageViewerLoadingIndicator loadingIndicator">
+            <div className="indicatorContents">
+                <h2>
+                    Loading...
+                    <i className="fa fa-spin fa-circle-o-notch fa-fw" />{' '}
+                </h2>
             </div>
-        )
-    }
+        </div>
+    )
 }
