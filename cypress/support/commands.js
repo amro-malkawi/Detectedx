4dd7963cfdfae47d50d031ecef1420021a81ef7f
Benz Natthakul <natthakul.b@aware.co.th>
2021-07-06 18:37:55 +0700

    Add getBySel, getBySelLike

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,11 @@
 
 
 import "./authentication/sign-in-with-email-password";
+
+Cypress.Commands.add('getBySel', (selector, ...args) => {
+    return cy.get(`[data-test=${selector}]`, ...args)
+  })
+  
+  Cypress.Commands.add('getBySelLike', (selector, ...args) => {
+    return cy.get(`[data-test*=${selector}]`, ...args)
+  })
