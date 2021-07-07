a8a50864fcc3c42047e88b8545bd41f0ae0d7a1c
Benz Natthakul <natthakul.b@aware.co.th>
2021-07-07 18:03:21 +0700

    Remove unused part

--- cypress/integration/4-list-page/instruction.spec.js
+++ cypress/integration/4-list-page/instruction.spec.js
@@ -1,17 +1,10 @@
 /// <reference types="cypress" />
 
-const apiHost = Cypress.env('apiUrl')
-const apiCouponInfo = {
-    method: 'GET',
-    url: `${apiHost}/coupons/**`,
-}
-
 context('Instruction', () => {
     describe('Expect to see the instruction detail', () => {
         beforeEach(() => {
             cy.loginWithEmailPassword(Cypress.env('test_username'), Cypress.env('test_password'));
             cy.visit('/app/test/list')
-            cy.waitForReact()
         })
         it('should see the instruction', () => {
             cy.getBySel('instruction-button').first().click().should('exist')
