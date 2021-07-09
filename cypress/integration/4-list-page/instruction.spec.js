6a798a0ffc24775adffe4e0bb44f349b9c937d08
Benz Natthakul <natthakul.b@aware.co.th>
2021-07-09 14:33:19 +0700

    Apply cypress-react-selector

--- cypress/integration/4-list-page/instruction.spec.js
+++ cypress/integration/4-list-page/instruction.spec.js
@@ -6,17 +6,23 @@ context('Instruction', () => {
         beforeEach(() => {
             cy.loginWithEmailPassword(Cypress.env('test_username'), Cypress.env('test_password'));
             cy.visit('/app/test/list')
+            cy.waitForReact()
+            cy.react('SwipeableViews')
         })
+
         it('should be able to click on modality tab', () => {
-            cy.getBySel('modality-tab-item').each(($el, index, $list) => {
-                cy.wrap($el).click().should('exist');
-            })
+
         })
-        it('can click instruction button see instruction text', () => {
-            cy.getBySel('modality-tab-item').each(($el, index, $list) => {
-                cy.wrap($el).children().contains('Instructions').click()
-                cy.get('.MuiDialogActions-root > div > .text-white').click().should('not.exist')
-            })
-        })
+        // it('should be able to click on modality tab', () => {
+        //     cy.getBySel('modality-tab-item').each(($el, index, $list) => {
+        //         cy.wrap($el).click().should('exist');
+        //     })
+        // })
+        // it('can click instruction button see instruction text', () => {
+        //     cy.getBySel('modality-tab-item').each(($el, index, $list) => {
+        //         cy.wrap($el).children().contains('Instructions').click().should('exist')
+        //         cy.get('.MuiDialogActions-root > div > .text-white').click().should('not.exist')
+        //     })
+        // })
     })
 })
