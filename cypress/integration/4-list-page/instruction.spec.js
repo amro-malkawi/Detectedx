27827e639feb5d1ddcb2bc9531a349dc0551ce77
Benz Natthakul <natthakul.b@aware.co.th>
2021-07-09 17:31:43 +0700

    Update instruction spec

--- cypress/integration/4-list-page/instruction.spec.js
+++ cypress/integration/4-list-page/instruction.spec.js
@@ -6,23 +6,41 @@ context('Instruction', () => {
         beforeEach(() => {
             cy.loginWithEmailPassword(Cypress.env('test_username'), Cypress.env('test_password'));
             cy.visit('/app/test/list')
-            cy.waitForReact(1000, '#root');
+            cy.waitForReact();
         })
 
-        it('should be able to click on modality tab', () => {
-            cy.wait(2000);
+        it('should be able to current state', () => {
+            cy.wait(500);
             cy.getReact('List', {options: {timeout: 5000}}).nthNode(2).getCurrentState()
         })
-        // it('should be able to click on modality tab', () => {
-        //     cy.getBySel('modality-tab-item').each(($el, index, $list) => {
-        //         cy.wrap($el).click().should('exist');
-        //     })
-        // })
-        // it('can click instruction button see instruction text', () => {
-        //     cy.getBySel('modality-tab-item').each(($el, index, $list) => {
-        //         cy.wrap($el).children().contains('Instructions').click().should('exist')
-        //         cy.get('.MuiDialogActions-root > div > .text-white').click().should('not.exist')
-        //     })
-        // })
+        it('should be able to click on modality tab', () => {
+            cy.getBySel('modality-tab-item').each(($el, index, $list) => {
+                cy.wrap($el).click().should('exist');
+            })
+        })
+
+        it('can click instruction button see instruction text', () => {
+            cy.getBySel('modality-tab-item').each(($el, index, $list) => {
+                cy.get($el).children().each((value) => {
+                    if (value[0]) {
+                        const result = value[0].attributes['data-cy']
+                        if (result) {
+                            cy.wrap($el)
+                                .children()
+                                .contains('Instructions')
+                                .should('have.length.at.least', 1)
+                                .click()
+                                .should('exist');
+                            cy.wait(1000)
+                            cy.get('.MuiDialogContent-root').scrollTo('bottom')
+                            cy.wait(1000)
+                            cy.get('.MuiDialogActions-root > div > .text-white')
+                                .click()
+                                .should('not.exist')
+                        }
+                    }
+                })
+            })
+        })
     })
 })
