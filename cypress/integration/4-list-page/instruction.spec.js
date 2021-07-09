89095adeae4c1a272614dbabcc164701e4180eea
shuli <andrei0807@hotmail.com>
2021-07-09 17:08:54 +0800

    cypress-react-selector

--- cypress/integration/4-list-page/instruction.spec.js
+++ cypress/integration/4-list-page/instruction.spec.js
@@ -6,12 +6,12 @@ context('Instruction', () => {
         beforeEach(() => {
             cy.loginWithEmailPassword(Cypress.env('test_username'), Cypress.env('test_password'));
             cy.visit('/app/test/list')
-            cy.waitForReact()
-            cy.react('SwipeableViews')
+            cy.waitForReact(1000, '#root');
         })
 
         it('should be able to click on modality tab', () => {
-
+            cy.wait(2000);
+            cy.getReact('List', {options: {timeout: 5000}}).nthNode(2).getCurrentState()
         })
         // it('should be able to click on modality tab', () => {
         //     cy.getBySel('modality-tab-item').each(($el, index, $list) => {
