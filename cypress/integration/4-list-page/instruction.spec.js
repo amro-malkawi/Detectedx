3f489c515a46f0b91b4e5e9e97c6946e7ccbf555
Benz Natthakul <natthakul.b@aware.co.th>
2021-07-12 14:40:27 +0700

    Update list page spec

--- cypress/integration/4-list-page/instruction.spec.js
+++ cypress/integration/4-list-page/instruction.spec.js
@@ -1,24 +1,24 @@
 /// <reference types="cypress" />
 
 let log = console.log
-context('Instruction', () => {
-    describe('Expect to see the instruction detail', () => {
+context('List Page', () => {
+    describe('Expect to see the list page functional', () => {
         before(() => {
             cy.loginWithEmailPassword(Cypress.env('test_username'), Cypress.env('test_password'));
             cy.visit('/app/test/list')
             cy.waitForReact();
         })
-
-        it('should be able to current state', () => {
+        it('should be able to get current state', () => {
             cy.wait(500);
             cy.getReact('List', {options: {timeout: 5000}}).nthNode(2).getCurrentState()
         })
-        it('should be able to click on modality tab', () => {
+        it('should be able to click on modality tab, and see modality description', () => {
             cy.getBySel('modality-tab-item').each(($el, index, $list) => {
                 cy.wrap($el).click().should('exist');
+                cy.wait(500)
+                cy.getBySel('modality-desc-text').should('exist')
             })
         })
-
         it('should be able to click the instruction button and see instruction text', () => {
             cy.getBySel('modality-tab-item').each(($el, index, $list) => {
                 cy.wait(500)
@@ -41,6 +41,20 @@ context('Instruction', () => {
                         }
                     }
                 })
+            })
+        })
+        it('should see the instruction video and able to play', () => {
+            cy.wait(1000)
+            cy.getBySel('instruction-video').each((value) => {
+                cy.wrap(value).click({force: true}).should('exist')
+                cy.wait(4500)
+                cy.get('.video-close-button > .zmdi').click().should('not.exist')
+            })
+        })
+        it('should see the modality test set', () => {
+            cy.getBySel('test-set').each((value) => {
+                cy.wait(200)
+                cy.get(value).scrollIntoView().should('exist')
             })
         })
     })
