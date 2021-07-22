cbdd9382970e89851c1b9987f4b7d83c2f361be9
Benz Natthakul <natthakul.b@aware.co.th>
2021-07-22 12:30:14 +0700

    Add - dropdown list constant - breasted mammography score spec

--- cypress/integration/5-score-page/breasted-mammography-score.spec.js.js
+++ cypress/integration/5-score-page/breasted-mammography-score.spec.js.js
@@ -1,3 +1,9 @@
+import { dropdownKey, dropdownValue } from '../../support/breasted-mammography/breasted-mammography-dropdown-list'
+const apiHost = Cypress.env('apiUrl')
+const apiSelectDrownDownList = {
+    method: 'GET',
+    url: `${apiHost}/scores/attempt_percentile**`
+}
 
 function getButtonByNameOfCard(card_name, button_name) {
     cy.getReact('CardBody').then((cards) => {
@@ -24,6 +30,17 @@ function navigateToScorePage() {
     getButtonByNameOfCard(selector.card, selector.button)
 }
 
+function clickViewButton() {
+    cy.get('button').contains('View').should('exist').click()
+}
+
+function interceptDropdownRequest() {
+    cy.intercept({
+        method: apiSelectDrownDownList.method,
+        url: apiSelectDrownDownList.url,
+    }).as("scoresAttemptPercentile");
+}
+
 context('Breasted Mammography - Score Page', () => {
     describe('Expect to see breasted mammography score page functional', () => {
         beforeEach(() => {
@@ -32,16 +49,59 @@ context('Breasted Mammography - Score Page', () => {
             cy.waitForReact()
             cy.contains('BreastED - Mammography').click();
             navigateToScorePage()
-            cy.wait(2000)
+            cy.wait(1000)
         })
         afterEach(() => {
-            cy.wait(3000)
+            cy.wait(1000)
         })
 
         it('should be able to click on view button and navigate to score page', () => {
-            cy.get('button').contains('View').should('exist').click()
+            clickViewButton()
             cy.get('.rct-page').should('be.visible')
             cy.get('.score-circle-container').should('be.visible')
+        })
+
+        it.only('should be able to re-select the drop down list on score page', () => {
+            clickViewButton()
+            interceptDropdownRequest()
+            const checkAllDropdownListAt = (number) => {
+                const selector = `:nth-child(${number}) > .score-chart-title > .form-control`
+                cy.get(selector)
+                    .should('exist')
+                    .and('be.visible')
+                    .select(dropdownKey.BreastPhysician)
+                    .invoke('val')
+                    .should('deep.equal', dropdownValue.BreastPhysician)
+                cy.get(selector)
+                    .should('exist')
+                    .and('be.visible')
+                    .select(dropdownKey.Radiologist)
+                    .invoke('val')
+                    .should('deep.equal', dropdownValue.Radiologist)
+                cy.get(selector)
+                    .should('exist')
+                    .and('be.visible')
+                    .select(dropdownKey.Radiographer)
+                    .invoke('val')
+                    .should('deep.equal', dropdownValue.Radiographer)
+                cy.get(selector)
+                    .should('exist')
+                    .and('be.visible')
+                    .select(dropdownKey.Trainee)
+                    .invoke('val')
+                    .should('deep.equal', dropdownValue.Trainee)
+                cy.get(selector)
+                    .should('exist')
+                    .and('be.visible')
+                    .select(dropdownKey.other)
+                    .invoke('val')
+                    .should('deep.equal', dropdownValue.other)
+
+                cy.wait('@scoresAttemptPercentile').its('response.statusCode').should('eq', 200)
+            }
+            checkAllDropdownListAt(1)
+            checkAllDropdownListAt(2)
+            checkAllDropdownListAt(3)
         })
     })
 })
---
+++ cypress/support/breasted-mammography/breasted-mammography-dropdown-list.js
@@ -0,0 +1,14 @@
+export const dropdownKey = {
+    BreastPhysician: 'Breast physician',
+    Radiologist: 'Radiologist',
+    Radiographer: 'Radiographer',
+    Trainee: 'Trainee',
+    other: 'other',
+}
+export const dropdownValue = {
+    BreastPhysician: 'cbc6aea0-d30b-11e9-af2c-54a050d2ebba',
+    Radiologist: 'cbc6b0c3-d30b-11e9-af2c-54a050d2ebba',
+    Radiographer: 'cbc6b123-d30b-11e9-af2c-54a050d2ebba',
+    Trainee: 'cbc6b16f-d30b-11e9-af2c-54a050d2ebba',
+    other: 'cbc6b1b1-d30b-11e9-af2c-54a050d2ebba',
+}
