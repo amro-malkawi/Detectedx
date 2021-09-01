7ef64dbc3fa60dcbb3c2b0bf1d722eb1eea92ac6
Benz Natthakul <natthakul.b@aware.co.th>
2021-09-01 17:58:10 +0700

    [AW-19] Optimize Chest Spec

--- cypress/integration/6-chest/chest.spec.js
+++ cypress/integration/6-chest/chest.spec.js
@@ -1,317 +1,67 @@
-import { selectChestConfidence, navigateToTestSet, loginWithEmailPasswordWithCookiesPreserved, waitLoadingResources } from "../../support/common/functions/index"
-import { TOOL } from "../../support/common/constants/index"
-
-const modality_name = 'CHEST'
-function getTool(toolName) {
-    cy.get(`[data-tool="${toolName}"]`).should('exist').and('be.visible').click()
+import { 
+    navigateToTestSet, 
+    loginWithEmailPasswordWithCookiesPreserved, 
+    waitLoadingResources, 
+    clickOnModalityTab 
+} from "../../support/common/functions/index"
+import { MODALITY_NAME, TOOL } from "../../support/common/constants/index"
+import { 
+    validateInstructionFeature, 
+    validateInvertFeature, 
+    validateNextPreviousFeature, 
+    validateSeriesFeature, 
+    validateTool 
+} from "../../support/common/functions/validation"
+import { validateHangingButtonFeature } from "../../support/chest/utils"
+const CURRENT_TEST = {
+    MODALITY_NAME: MODALITY_NAME.Chest
 }
-context('Test Page - Chest', () => {
-    describe('Expect to see Chest modality functional', () => {
+context(`Test Page - ${CURRENT_TEST.MODALITY_NAME}`, () => {
+    describe(`Expect to see ${CURRENT_TEST.MODALITY_NAME} modality functional`, () => {
         before(() => {
             loginWithEmailPasswordWithCookiesPreserved()
         })
         beforeEach(() => {
             cy.visit('/app/test/list')
-            
-            cy.contains(modality_name).should('be.visible').click();
-            navigateToTestSet(modality_name)
+            clickOnModalityTab(CURRENT_TEST.MODALITY_NAME)
+            navigateToTestSet(CURRENT_TEST.MODALITY_NAME)
         })
-
         it('should be able to load all images', () => {
             waitLoadingResources()
         })
-        it('should be able to use series feature', () => {
-            const defaultTime = 2000
-            const scrolling = () => {
-                cy.get('.image-browser')
-                    .scrollTo('bottom', { duration: defaultTime })
-                cy.get('.image-browser')
-                    .scrollTo('top', { duration: defaultTime })
-            }
-            const toggleSeriesIcon = () => {
-                cy.get('.series-icon').click()
-            }
-            toggleSeriesIcon()
-            scrolling()
-            cy.wait(1000)
-            toggleSeriesIcon()
+        it('should be able to use Series feature', () => {
+            validateSeriesFeature()
         })
         it('should be able to use hanging button', () => {
-            const selectFirst = () => {
-                cy.get('.hanging-type-container').click().should('exist')
-                cy.get('.MuiList-root > :nth-child(1)').click()
-                cy.get('.image-row').then((row) => {
-                    expect(row.length).to.eq(1)
-                    expect(row[0].childElementCount).to.eq(1)
-                })
-            }
-            const selectSecond = () => {
-                cy.get('.hanging-type-container').click().should('exist')
-                cy.get('.MuiList-root > :nth-child(2)').click().should('exist')
-                cy.get('.image-row').then((row) => {
-                    expect(row.length).to.eq(1)
-                    expect(row[0].childElementCount).to.eq(2)
-                })
-            }
-            selectSecond()
-            cy.wait(2000)
-            selectFirst()
+            validateHangingButtonFeature()
         })
-        it('should be able to use instruction', () => {
-            cy.get('button')
-                .contains('Instruction')
-                .click()
-                .should('exist')
-            cy.get('.MuiDialogContent-root')
-                .scrollTo('bottom', { duration: 3000 })
-            cy.get('.MuiDialogContent-root')
-                .scrollTo('top', { duration: 3000 })
-            cy.get('button')
-                .contains('Close')
-                .click()
-                .should('not.exist')
+        it('should be able to use Instruction', () => {
+            validateInstructionFeature()
         })
-        it('should be able to use Sync function', () => {
-            const toggleSyncSwitch = () => {
-                cy.getBySel('synchronizer-switch').click()
+        it('should be able to use Next, Previous test case feature', () => {
+            const options = {
+                modalityName: CURRENT_TEST.MODALITY_NAME,
+                selectConfidence: true,
             }
-            const selectSecondCase = () => {
-                cy.get('.hanging-type-container').click().should('exist')
-                cy.get('.MuiList-root > :nth-child(2)').click().should('exist')
-                cy.get('.image-row').then((row) => {
-                    expect(row.length).to.eq(1)
-                    expect(row[0].childElementCount).to.eq(2)
-                })
-            }
-            toggleSyncSwitch()
-            selectSecondCase()
-            getTool(TOOL.ZOOM)
-            const zoomAction = (row) => {
-                const pageX = 178
-                const move = (row, i) => {
-                    cy.wrap(row[0].childNodes[0])
-                        .trigger('mousedown', { which: 1, pageX: pageX, pageY: i })
-                        .trigger('mousemove', { which: 1, pageX: pageX, pageY: i })
-                }
-                const smooth = 0.8;
-                const zoom = {
-                    in: {
-                        start: 500,
-                        end: 520
-                    },
-                    out: {
-                        start: 520,
-                        end: 512
-                    }
-                }
-                for (let i = zoom.in.start; i < zoom.in.end; i = i + smooth) {
-                    move(row, i)
-                }
-                for (let i = zoom.out.start; i >= zoom.out.end; i = i - smooth) {
-                    move(row, i)
-                }
-            }
-            cy.wait(3000)
-            cy.get('.image-row').then((row) => {
-                zoomAction(row)
-            })
-        })
-
-        it('should be able to use next, previous test case feature', () => {
-            const testCaseValue = String(1)
-            cy.get('select')
-                .select(testCaseValue)
-            cy.get('select')
-                .should('have.value', testCaseValue).and('exist').and('be.visible')
-            
-            selectChestConfidence(3)
-
-            cy.get('button')
-                .contains('Next')
-                .click()
-            cy.get('select')
-                .should('have.value', Number(testCaseValue) + 1)
-            cy.get('button')
-                .contains('Previous')
-                .click()
-            cy.get('select')
-                .should('have.value', Number(testCaseValue))
+            validateNextPreviousFeature(options)
         })
         it('should be able to use Pan tool', () => {
-            getTool(TOOL.PAN)
-            const panAction = (row) => {
-                const pageX = 600
-                const move = (row, i) => {
-                    cy.wrap(row[0].childNodes[0])
-                        .trigger('mousedown', { which: 1, pageX: pageX, pageY: i })
-                        .trigger('mousemove', { which: 1, pageX: pageX, pageY: i })
-                }
-                const smooth = 50;
-                const pan = {
-                    up: {
-                        start: 75,
-                        end: 250
-                    },
-                    down: {
-                        start: 250,
-                        end: 100
-                    }
-                }
-                for (let i = pan.up.start; i < pan.up.end; i = i + smooth) {
-                    move(row, i)
-                }
-                for (let i = pan.down.start; i > pan.down.end; i = i - smooth) {
-                    move(row, i)
-                }
-            }
-            cy.get('.image-row').then((row) => {
-                panAction(row)
-            })
+            validateTool(TOOL.PAN)
         })
-
         it('should be able to use Zoom tool', () => {
-            getTool(TOOL.ZOOM)
-            const zoomAction = (row) => {
-                const pageX = 450
-                const move = (row, i) => {
-                    cy.wrap(row[0].childNodes[0])
-                        .trigger('mousedown', { which: 1, pageX: pageX, pageY: i })
-                        .trigger('mousemove', { which: 1, pageX: pageX, pageY: i })
-                }
-                const smooth = 0.8;
-                const zoom = {
-                    in: {
-                        start: 500,
-                        end: 520
-                    },
-                    out: {
-                        start: 520,
-                        end: 512
-                    }
-                }
-                for (let i = zoom.in.start; i < zoom.in.end; i = i + smooth) {
-                    move(row, i)
-                }
-                for (let i = zoom.out.start; i >= zoom.out.end; i = i - smooth) {
-                    move(row, i)
-                }
-            }
-            cy.get('.image-row').then((row) => {
-                zoomAction(row)
-            })
+            validateTool(TOOL.ZOOM)
         })
         it('should be able to use Window tool', () => {
-            getTool(TOOL.WINDOW)
-            const windowAction = (row) => {
-                const element = row[0].childNodes[0]
-                const pageX = 0
-                const pageY = 0
-                const moveY = (element, i) => {
-                    cy.wrap(element)
-                        .trigger('mousedown', { which: 1, pageX: pageX, pageY: i })
-                        .trigger('mousemove', { which: 1, pageX: pageX, pageY: i })
-                    }
-                    const moveX = (element, i) => {
-                        cy.wrap(element)
-                        .trigger('mousedown', { which: 1, pageX: i, pageY: pageY })
-                        .trigger('mousemove', { which: 1, pageX: i, pageY: pageY })
-                }
-                const smooth = 10;
-                const window = {
-                    y: {
-                        in: {
-                            start: 1,
-                            end: 80
-                        },
-                        out: {
-                            start: 80,
-                            end: 1
-                        }
-                    },
-                    x: {
-                        in: {
-                            start: 1,
-                            end: 80
-                        },
-                        out: {
-                            start: 80,
-                            end: 1
-                        }
-                    }
-                }
-                for (let i = window.y.in.start; i < window.y.in.end; i = i + smooth) {
-                    moveY(element, i)
-                }
-                for (let i = window.y.out.start; i >= window.y.out.end; i = i - smooth) {
-                    moveY(element, i)
-                }
-                
-                cy.wrap(element).trigger('mouseup')
-
-                for (let i = window.x.in.start; i < window.x.in.end; i = i + smooth) {
-                    moveX(element, i)
-                }
-                for (let i = window.x.out.start; i >= window.x.out.end; i = i - smooth) {
-                    moveX(element, i)
-                }
-            }
-            cy.get('.image-row').then((row) => {
-                windowAction(row)
-            })
+            validateTool(TOOL.WINDOW)
         })
         it('should be able to use Reset tool', () => {
-            getTool(TOOL.ZOOM)
-            const zoomAction = (row) => {
-                const pageX = 450
-                const move = (row, i) => {
-                    cy.wrap(row[0].childNodes[0])
-                        .trigger('mousedown', { which: 1, pageX: pageX, pageY: i })
-                        .trigger('mousemove', { which: 1, pageX: pageX, pageY: i })
-                }
-                const smooth = 0.8;
-                const zoom = {
-                    in: {
-                        start: 500,
-                        end: 520
-                    },
-                    out: {
-                        start: 520,
-                        end: 512
-                    }
-                }
-                for (let i = zoom.in.start; i < zoom.in.end; i = i + smooth) {
-                    move(row, i)
-                }
-            }
-            cy.get('.image-row').then((row) => {
-                zoomAction(row)
-            })
-            getTool(TOOL.RESET)
+            validateTool(TOOL.RESET)
         })
         it('should be able to use Length tool', () => {
-            getTool(TOOL.LENGTH)
-            const makeLength = (image) => {
-                cy.wrap(image)
-                    .trigger('mousedown', { which: 1, pageX: 366, pageY: 272 })
-                    .trigger('mousemove', { which: 1, pageX: 228, pageY: 326 })
-                    .trigger('mouseup', { which: 1, pageX: 228, pageY: 326 })
-            }
-            cy.get('.image-row').then((row) => {
-                const image = row[0].childNodes[0]
-                cy.wrap(image).dblclick()
-                makeLength(image)
-            })
+            validateTool(TOOL.LENGTH)
         })
-        it('should be able to use invert feature', () => {
-            const toggleInvertAction = () => {
-                cy.getBySel('tool-invert').should('be.visible').first().click();
-            }
-            cy.get('.image-row').then((row) => {
-                const image = row[0].childNodes[0]
-                cy.wrap(image).dblclick()
-                toggleInvertAction()
-                toggleInvertAction()
-            })
+        it('should be able to use Invert feature', () => {
+            validateInvertFeature()
         })
     })
 })
