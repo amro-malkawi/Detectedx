509d029fb7258ad80bd5cd8def38f57f88c51429
shuli <andrei0807@hotmail.com>
2022-07-31 13:25:21 +0800

    RIV-126

--- src/routes/test-view/lib/tools/ZoomMouseWheelTool.js
+++ src/routes/test-view/lib/tools/ZoomMouseWheelTool.js
@@ -80,31 +80,33 @@ export default class ZoomMouseWheelTool extends BaseTool {
             evt.detail.imageX,
             evt.detail.imageY,
         ];
-
+        const previousX = viewport.translation.x;
+        const previousScale = viewport.scale;
         const updatedViewport = this.changeViewportScale(viewport, ticks, {
             maxScale,
             minScale,
         });
-
         cornerstone.setViewport(element, updatedViewport);
 
-        // Now that the scale has been updated, determine the offset we need to apply to the center so we can
-        // Keep the original start location in the same position
+        // fix for RIV-126
         const newCoords = cornerstone.pageToPixel(element, startX, startY);
-
-        // The shift we will use is the difference between the original image coordinates of the point we've selected
-        // And the image coordinates of the same point on the page after the viewport scaling above has been performed
-        // This shift is in image coordinates, and is designed to keep the target location fixed on the page.
         let shift = {
             x: imageX - newCoords.x,
             y: imageY - newCoords.y,
         };
-
-        // Correct the required shift using the viewport rotation and flip parameters
         shift = this.correctShift(shift, updatedViewport);
-
         // Apply the shift to the Viewport's translation setting
-        viewport.translation.x -= shift.x;
         viewport.translation.y -= shift.y;
+        const hangingId = element.parentElement.dataset.hangingId;
+        const imageWidth = viewport.displayedArea.brhc.x;
+        const canvasWidth = element.clientWidth;
+        if(hangingId && hangingId.split('-')[1]) {
+            if(hangingId.split('-')[1] === 'L') {
+                updatedViewport.translation.x = (imageWidth - canvasWidth / updatedViewport.scale) / 2 - ((imageWidth - canvasWidth / previousScale) / 2 - previousX);
+            } else if(hangingId.split('-')[1] === 'R') {
+                // updatedViewport.translation.x = -((imageWidth - canvasWidth / updatedViewport.scale) / 2);
+                updatedViewport.translation.x = -(((imageWidth + canvasWidth / previousScale) / 2 - previousX) - imageWidth / 2 - (canvasWidth / updatedViewport.scale) / 2);
+            }
+        }
     }
 }
