a4c3275099c167b6ddb5854e5fdff02c853b0d3d
Viktor Angeliev <viktor.angeliev@primeholding.com>
2023-02-27 19:44:31 +0200

    Add freetext questions

--- src/routes/test-view/component/SideQuestions/DynamicQuestions/DynamicQuestion.js
+++ src/routes/test-view/component/SideQuestions/DynamicQuestions/DynamicQuestion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DynamicQuestionMulti } from './DynamicQuestionMulti';
 import { DynamicQuestionSingle } from './DynamicQuestionSingle';
+import { DynamicQuestionFreetext } from './DynamicQuestionFreetext';
 
 export const DynamicQuestion = ({
     question,
@@ -8,11 +9,14 @@ export const DynamicQuestion = ({
     truth,
     onChange
 }) => {
-    if (question.type === 'single') {
-        return <DynamicQuestionSingle question={question} answer={answer} truth={truth} onChange={onChange}/>;
-    } else if (question.type === 'multi') {
-        return <DynamicQuestionMulti question={question} answer={answer} truth={truth} onChange={onChange}/>;
+    switch (question.type) {
+        case 'single':
+            return <DynamicQuestionSingle question={question} answer={answer} truth={truth} onChange={onChange}/>;
+        case 'multi':
+            return <DynamicQuestionMulti question={question} answer={answer} truth={truth} onChange={onChange}/>;
+        case 'freetext':
+            return <DynamicQuestionFreetext question={question} answer={answer} truth={truth} onChange={onChange}/>;
+        default:
+            return null;
     }
-
-    return null;
 };
